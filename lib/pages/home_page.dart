6ffbfc0afa1c9d9bcbc58276044d2e70a619596f
ReapersSchythe <110484207+ReapersSchythe@users.noreply.github.com>
2024-12-29 21:56:58 +0530

    Add files via upload

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/util/dialog_box.dart';
+import 'package:todoapp/util/todo_tiles.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  final _controller= TextEditingController();
+  List toDoList = [
+    ["Make dinner", false],
+    ["Exercise", false] 
+  ];
+
+  void checkBoxChanged(bool? value, int index){
+    setState(() {
+      toDoList[index][1]=!toDoList[index][1];
+    });
+  }
+  void saveNewTask(){
+    setState(() {
+      toDoList.add([_controller.text,false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+  void createNewTask(){
+    showDialog(
+      context: context,
+      builder: (context){
+        return DialogBox(
+          controller: _controller,
+          onSave:saveNewTask,
+          onCancel: ()=> Navigator.of(context).pop(),
+        );
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Text("TO DO"),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context ,index){
+          return TodoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1], 
+            onChanged: (value)=>checkBoxChanged(value,index),
+          );
+        },
+// yes
+      ),
+    );
+  }
+}
